ace8f13be998a10e8f5f741ec0fa018d830a287a
Liu-Sitong <liusit@foxmail.com>
2018-07-12 17:22:37 +0800

    pokemon_gd2使用二次函数线性回归
    cost加了log，略改了绘图部分

--- pokemon_gd2.py
+++ pokemon_gd2.py
@@ -7,21 +7,23 @@ pokemon_dataframe = pd.read_csv('pokemon.csv', sep=',')
 pokemon_dataframe = pokemon_dataframe.reindex(np.random.permutation(pokemon_dataframe.index))
 
 train_size = 50
-iteration = 1000
+iteration = 15000
+lr = 20  # learning rate
 # para_num =
 
 X = tf.placeholder(tf.float32, [None, 1])
 w = tf.Variable(tf.zeros([1, 1]))
 w2 = tf.Variable(tf.zeros([1, 1]))
+w3 = tf.Variable(tf.zeros([1, 1]))
 b = tf.Variable(tf.zeros([1]))
-y = tf.matmul(tf.square(X), w2) + tf.matmul(X, w) + b
+y = tf.matmul(tf.pow(X, 3), w3) + tf.matmul(tf.square(X), w2) + tf.matmul(X, w) + b
 Y = tf.placeholder(tf.float32, [None, 1])
 
 # 成本函数 sum(sqr(y_-y))/n
 cost = tf.reduce_mean(tf.square(Y - y))
 
 # 用梯度下降训练
-train_step = tf.train.AdagradOptimizer(5).minimize(cost)
+train_step = tf.train.AdagradOptimizer(lr).minimize(cost)
 
 init = tf.initialize_all_variables()
 sess = tf.Session()
@@ -41,23 +43,28 @@ for i in range(iteration):
 
 w2_result = sess.run(w2)
 w_result = sess.run(w)
+w3_result = sess.run(w3)
 b_result = sess.run(b)
 print("w:%f" % w_result)
 print("w2:%f" % w2_result)
+print("w3:%f" % w3_result)
 print("b:%f" % b_result)
 print("train cost:%f" % sess.run(cost, feed_dict={X: x_train, Y: y_train}))
 print("test cost:%f" % sess.run(cost, feed_dict={X: x_test, Y: y_test}))
 
 # print(history_w)
-plt.plot(range(len(history_cost)), history_cost)
+plt.plot(range(len(history_cost)), np.log(history_cost))
 plt.title('change of cost')
-plt.ylabel("cost")
+plt.ylabel("log cost")
 plt.xlabel("iteration")
 plt.savefig('cost_change.jpg')
 plt.show()
 
-plt.plot(x_test, y_test, '.')
-maxx = max(x_train)[0] * 1.1
+#
+plt.plot(x_test, y_test, '.r')
+plt.plot(x_train, y_train, '.b')
+plt.title('red:test; blue:train')
+maxx = max(x_train + x_test)[0] * 1.1
 
 x_space = np.linspace(0, maxx, maxx * 10)
 plt.ylabel("b")
